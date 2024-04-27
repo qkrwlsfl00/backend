f9bb9f4181ccd711831e1d93ea5d71ac5118dc1f
unknown <qkrwlsfl00@o365.yonsei.ac.kr>
2024-04-27 23:01:09 +0300

    database applied

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+require('dotenv').config()
+
+const Note = require('./models/note')
+
+app.use(express.static('dist'))
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -10,89 +15,98 @@ const requestLogger = (request, response, next) => {
   next()
 } 
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-app.use(requestLogger)
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  next(error)
 }
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json()) //must be before requestLogger; otherwise request.body would be 'undefined'
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-  response.json(note) 
-  } else {
-    response.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+//cannot use delete now
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
+  
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-
+  
   if (!body.content) {
     return response.status(400).json({  //return of status(): response -> can use .json() method 
       error: 'content missing'
     })
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  }
-  
-  notes = notes.concat(note)
-  console.log(note);
-  response.json(note)
+  })
+  note.save().then(savedNote => {
+    console.log("note saved!", savedNote);
+    response.json(savedNote) //put this in 'then'! otherwise it responses normally when error
+  })
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+//handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+//handler of requests with result to errors
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) { 
+  console.log('give password as argument');
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://qkrwlsfl00:${password}@cluster0.xm2iqb4.mongodb.net/?
+  retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+
+note.save().then(result => {
+  console.log('note saved!:', result);
+  mongoose.connection.close()
+})
+
+/*
+Note.find({ important: true }).then(result => {
+  result.forEach(note => {
+    console.log(note);
+  })
+  mongoose.connection.close()
+})
+*/
